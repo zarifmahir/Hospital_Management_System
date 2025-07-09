b1e867892309aff4560013bd652be7466c5c0dc9
zarifmahir <zarifmahir21@gmail.com>
2025-07-09 23:23:17 +0600

    practice push3

--- src/main/java/com/example/hospital_management_system/HelloApplication.java
+++ src/main/java/com/example/hospital_management_system/HelloApplication.java
@@ -14,6 +14,7 @@ public class HelloApplication extends Application {
         Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
         stage.setTitle("Hello!");
         System.out.println("hello world");
+        System.out.println("practice push 3");
         stage.setScene(scene);
         stage.show();
     }
