001f5464487db2c2cd90821bc6b44ffa1c72e5b2
zarifmahir <zarifmahir21@gmail.com>
2025-07-09 22:56:08 +0600

    loginIn page demo

--- src/main/java/com/example/hospital_management_system/LoginController.java
+++ src/main/java/com/example/hospital_management_system/LoginController.java
@@ -1,4 +1,32 @@
 package com.example.hospital_management_system;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+
 public class LoginController {
+    @FXML
+    private TextField username;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private Button LogInButton;
+
+    @FXML
+    private Label errorMessage;
+
+    public void userLogIn(ActionEvent actionEvent) throws IOException {
+        checkLogin();
+    }
+
+    private void checkLogin() throws IOException {
+
+    }
 }
